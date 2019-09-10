f30df390c926748b1512dcd7e565a5be6a88d2a6
Vico <vinicomelo@gmail.com>
2019-09-09 22:27:30 -0400

    skeleton

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: Card(
+                child: Text('CHART'),
+              ),
+              width: double.infinity,
+            ),
+            Card(
+              child: Text('LIST OF TX'),
+            ),
+          ],
+        ));
+  }
+}
