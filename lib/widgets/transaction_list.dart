c9728da6e69d92a7edc8e7030a93dcfb0ae73d2f
Vico Melo <vicomelo@vicos-mbp.lan>
2019-09-21 10:39:29 -0400

    added delete button

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -45,48 +45,13 @@ class TransactionList extends StatelessWidget {
                     subtitle: Text(
                       DateFormat.yMMMMd().format(transactions[idx].date),
                     ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {},
+                      color: Theme.of(context).errorColor,
+                    ),
                   ),
                 );
-                // return Card(
-                //   child: Row(
-                //     children: <Widget>[
-                //       Container(
-                //         margin: EdgeInsets.symmetric(
-                //           vertical: 10,
-                //           horizontal: 15,
-                //         ),
-                //         decoration: BoxDecoration(
-                //           border: Border.all(
-                //               color: Theme.of(context).primaryColor, width: 2),
-                //         ),
-                //         padding: EdgeInsets.all(10),
-                //         child: Text(
-                //           '\$ ${transactions[idx].amount.toStringAsFixed(2)}',
-                //           style: TextStyle(
-                //             fontWeight: FontWeight.bold,
-                //             fontSize: 20,
-                //             color: Theme.of(context).primaryColor,
-                //           ),
-                //         ),
-                //       ),
-                //       Column(
-                //         crossAxisAlignment: CrossAxisAlignment.start,
-                //         children: <Widget>[
-                //           Text(
-                //             transactions[idx].title,
-                //             style: Theme.of(context).textTheme.title,
-                //           ),
-                //           Text(
-                //             DateFormat.yMMMMd().format(transactions[idx].date),
-                //             style: TextStyle(
-                //               color: Colors.grey,
-                //             ),
-                //           )
-                //         ],
-                //       ),
-                //     ],
-                //   ),
-                // );
               },
               itemCount: transactions.length,
             ),
